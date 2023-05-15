a20e0158b800cbd4686be34ed4872de2a10d9ec1
pabloariel413 <pabloariel413@gmail.com>
2023-05-15 05:35:52 -0300

    security service agregado

--- src/main/java/com/portfolio/pablo/Security/Service/UsuarioService.java
+++ src/main/java/com/portfolio/pablo/Security/Service/UsuarioService.java
@@ -1,5 +1,8 @@
 package com.portfolio.pablo.Security.Service;
 
+import com.portfolio.pablo.Security.Entity.Usuario;
+import com.portfolio.pablo.Security.Repository.iUsuarioRepository;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,9 +10,24 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class UsuarioService {
+  @Autowired
+  iUsuarioRepository iusuarioRepository;  
+    
+  public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+      return iusuarioRepository.findByNombreUsuario(nombreUsuario);
+  }
   
-    
-    
+  public boolean existsByNombreUsuario(String nombreUsuario){
+      return iusuarioRepository.existsByNombreUsuario(nombreUsuario);
+  }
+  
+  public boolean existsByEmail(String email){
+      return iusuarioRepository.existsByEmail(email); 
+  }
+  
+  public void save(Usuario usuario){
+      iusuarioRepository.save(usuario);
+  }
 }
 
    
